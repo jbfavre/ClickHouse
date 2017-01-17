44e43784fc23672a2a40975aa3a0c7ab45d46be4
Jean Baptiste Favre <debian@jbfavre.org>
2017-01-17 15:00:00 +0100

    Enable tests from Debian build

--- debian/tests_wrapper.sh
+++ debian/tests_wrapper.sh
@@ -3,21 +3,28 @@
 set -o errexit
 set -o pipefail
 
+ln -sf clickhouse build/dbms/src/Server/clickhouse-server
+ln -sf clickhouse build/dbms/src/Server/clickhouse-client
+ln -sf clickhouse build/dbms/src/Server/clickhouse-local
 # Start a local clickhouse server which will be used to run tests
 PWD=$(pwd)
-cd debian/tmp
-PATH=$PATH:/home/dev/Debian/clickhouse/debian/tmp/usr/bin \
-  ./usr/bin/clickhouse-server --config-file=../clickhouse-server-config-tests.xml
+echo ${PWD}
+PATH=$PATH:./build/dbms/src/Server \
+  ./build/dbms/src/Server/clickhouse-server --config-file=./debian/clickhouse-server-config-tests.xml 2>/dev/null &
 CH_PID=$!
-cd ${PWD}
 
 # Define needed stuff to kill test clickhouse server after tests completion
 function finish {
   kill $CH_PID
   wait
+  rm -rf /tmp/clickhouse
+  rm -f build/dbms/src/Server/clickhouse-local
+  rm -f build/dbms/src/Server/clickhouse-client
+  rm -f build/dbms/src/Server/clickhouse-server
 }
 trap finish EXIT
 
 # Do tests
-PATH=$PATH:/home/dev/Debian/clickhouse/debian/tmp/usr/bin \
-  ./dbms/tests/clickhouse-test -c debian/tmp/usr/bin/clickhouse-client
+cd dbms/tests
+PATH=$PATH:../../build/dbms/src/Server \
+  ./clickhouse-test -c ../../build/dbms/src/Server/clickhouse-client
